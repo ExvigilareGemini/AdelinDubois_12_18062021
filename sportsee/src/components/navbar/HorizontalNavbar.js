da7ddb17cb859094ac3a7e33d0688ac5114456c9
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-06-18 20:02:02 +0200

    CRLF troubleshooting + navbar init

---
+++ sportsee/src/components/navbar/HorizontalNavbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HorizontalNavbar() {
+	return (
+		<nav className="HorizontalNavbar">
+			<img src="%PUBLIC_URL%/logo.svg" alt="banner" className="bannerImage"></img>
+		</nav>
+	);
+}
+
+export default HorizontalNavbar;
---
+++ sportsee/src/components/navbar/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HorizontalNavbar from './HorizontalNavbar';
+
+function Nav() {
+	return (
+		<nav className="Nav">
+			<HorizontalNavbar />
+		</nav>
+	);
+}
+
+export default Nav;
