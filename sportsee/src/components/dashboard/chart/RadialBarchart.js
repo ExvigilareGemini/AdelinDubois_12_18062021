e0ac3577d8935f6a7dbbacc9e745dc3db4d4c6aa
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-07-11 17:45:29 +0200

    Radialbarchart layout

--- sportsee/src/components/dashboard/chart/RadialBarchart.js
+++ sportsee/src/components/dashboard/chart/RadialBarchart.js
@@ -1,25 +1,64 @@
 import React from "react";
-import RadialBarchartLabel from "./Label/RadialBarchartLabel";
 import {
   RadialBarChart,
   RadialBar,
   PolarAngleAxis,
   ResponsiveContainer,
   LabelList,
-  Legend
+  Legend,
 } from "recharts";
 
 export class RadialBarchart extends React.Component {
   render() {
+    const dataPercentage = [
+      { todayScore: this.props.todayScore, fill: "#E60000" },
+    ];
+
     return (
-      <ResponsiveContainer className="radialBarchart" width="32%" height="100%">
-        <RadialBarChart cx="50%" cy="50%" innerRadius="80%" outerRadius="90%" barSize={20} data={[{ todayScore: this.props.todayScore, fill: "#E60000", name: "Score" }]} startAngle={90} endAngle={450}>
-          <PolarAngleAxis type="number" domain={[0, 1]} tick={false} background />
-          <LabelList dataKey="Test" />
-          <RadialBar dataKey="todayScore" label={<RadialBarchartLabel />} />
-          <Legend align="left" verticalAlign="top" iconSize="0" fill="#ffffff" />
-        </RadialBarChart>
-      </ResponsiveContainer>
+      <>
+        <h2 className="radialBarchart_title">Score</h2>
+        <p className="radialBarchart_text">
+          {dataPercentage[0].todayScore * 100}% <br></br>
+          <span className="radialBarchart_text_small">de votre objectif</span>
+        </p>
+        <ResponsiveContainer
+          className="radialBarchart"
+          width="32%"
+          height="100%"
+        >
+          <RadialBarChart
+
+            cx="50%"
+            cy="50%"
+            innerRadius="80%"
+            outerRadius="90%"
+            barSize={20}
+            data={dataPercentage}
+            startAngle={90}
+            endAngle={450}
+          >
+            <PolarAngleAxis
+              type="number"
+              domain={[0, 1]}
+              tick={false}
+              background
+            />
+            <LabelList dataKey="Test" />
+            <RadialBar
+              dataKey="todayScore"
+              stroke="red"
+              fill="red"
+              cornerRadius={10}
+            />
+            <Legend
+              align="left"
+              verticalAlign="top"
+              iconSize="0"
+              fill="#ffffff"
+            />
+          </RadialBarChart>
+        </ResponsiveContainer>
+      </>
     );
   }
 }
